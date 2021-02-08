2b7b3791a158c16657f2ccb22f1427a45dcdfa16
旭晓耿 <gengxiaoxu@5kcrm.com>
2021-02-08 13:26:13 +0800

    v11.0.0

--- public/sql/5kcrm.sql
+++ public/sql/5kcrm.sql
@@ -1957,3 +1957,6 @@ CREATE TABLE `5kcrm_crm_receivables_file` (
   PRIMARY KEY (`r_id`) USING BTREE
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
+INSERT INTO `5kcrm_crm_config` (`name`, `value`, `description`) VALUES ('activity_phrase', 'a:5:{i:0;s:18:\\\"电话无人接听\\\";i:1;s:15:\\\"客户无意向\\\";i:2;s:42:\\\"客户意向度适中，后续继续跟进\\\";i:3;s:42:\\\"客户意向度较强，成交几率较大\\\";i:4;s:3:\\\"312\\\";}', '跟进记录常用语');
+INSERT INTO `5kcrm_crm_config` (`name`, `value`, `description`) VALUES ('visit_config', '1', '是否开启回访提醒：1开启；0不开启');
+INSERT INTO `5kcrm_crm_config` (`name`, `value`, `description`) VALUES ('visit_day', '10', '客户回访提醒天数');
